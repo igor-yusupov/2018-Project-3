c201688807e5fe692930e225fd8fb9f64189d6b5
gmorgachev <morgachev.gi@phystech.edu>
2018-10-13 01:04:13 +0300

    images + simpliest iterator (with splitter)

---
+++ code/data_processing.py
@@ -0,0 +1,23 @@
+import numpy as np
+import pandas as pd
+
+class DataIterator(StopIteration):
+
+    def __init__(self, data, element_length, shuffle=True):
+        self.data = data
+        self.element_length = element_length
+        sample_size = data.shape[0] // element_length
+        self.starts = np.linspace(0, (sample_size - 1) * element_length, sample_size, dtype=int)
+        self.position = 0
+        if shuffle:
+            np.random.shuffle(self.starts)
+
+    def __iter__(self):
+        return self
+
+    def __next__(self):
+        if  self.position < self.starts.shape[0]:
+            start = self.starts[self.position]
+            self.position += 1
+            return self.data.loc[start:start + self.element_length - 1, :]
+        raise StopIteration()
