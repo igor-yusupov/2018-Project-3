c2d841dd119ed70b6a3c694e2ed0a15b9f6e26e5
Smirnov Vladislav <smirnovvlad2103@gmail.com>
2018-10-14 16:18:22 +0300

    add LN

--- code/dtw.py
+++ code/dtw.py
@@ -42,6 +42,37 @@ def dtw(x, y, dist, warp=1):
         path = _traceback(D0)
     return D1[-1, -1] / sum(D1.shape), C, D1, path
 
+def dtw_ln(x, y, dist, warp=1, l=0.3):
+    r, c = len(x), len(y)
+    lc = int(round(c * l * 0.5))
+    D0 = zeros((r + 1, c + 1))
+    D0[0, 1:] = inf
+    D0[1:, 0] = inf
+    D1 = D0[1:, 1:]  # view
+    for i in range(r):
+        for j in range(c):
+            if (c >= r - lc and c <= r + lc):
+                D1[i, j] = dist(x[i], y[j])
+            else:
+                D1[i, j] = inf
+    C = D1.copy()
+    for i in range(r):
+        for j in range(c):
+            min_list = [D0[i, j]]
+            for k in range(1, warp + 1):
+                i_k = min(i + k, r - 1)
+                j_k = min(j + k, c - 1)
+                min_list += [D0[i_k, j], D0[i, j_k]]
+            D1[i, j] += min(min_list)
+    if len(x)==1:
+        path = zeros(len(y)), range(len(y))
+    elif len(y) == 1:
+        path = range(len(x)), zeros(len(x))
+    else:
+        path = _traceback(D0)
+    return D1[-1, -1] / sum(D1.shape), C, D1, path
+
+
 
 def accelerated_dtw(x, y, dist, warp=1):
     """
