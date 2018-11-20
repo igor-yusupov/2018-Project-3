4a5f0c8a856062fcd082c123240c0db28f362179
gleb <morgachev.gi@phystech.edu>
2018-11-20 16:40:09 +0300

    added pseudo-random generator

--- code/data_processing.py
+++ code/data_processing.py
@@ -1,4 +1,3 @@
-from random import choice
 
 import numpy as np
 import pandas as pd
@@ -8,19 +7,16 @@ from sklearn.utils import shuffle as shuffle_set
 
 class DataIterator(StopIteration):
 
-    def __init__(self, data: pd.DataFrame, element_length, shuffle=True, overlap=0, random_state=-1):
+    def __init__(self, data: pd.DataFrame, element_length, shuffle=True, overlap=0, random_state=None):
+        self.pseudo_random_gen = np.random.RandomState(random_state)
         self.data = data
         self.element_length = element_length
         unique_element_length = element_length - overlap
         sample_size = (data.shape[0] - overlap) // unique_element_length
         self.starts = np.linspace(0, (sample_size - 1) * unique_element_length, sample_size, dtype=int)
         self.position = 0
-        self.random_state = random_state
         if shuffle:
-            if random_state != -1:
-                shuffle_set(self.starts, random_state=random_state)
-            else:
-                shuffle_set(self.starts)
+            self.pseudo_random_gen.shuffle(self.starts)
 
     def __iter__(self):
         return self
@@ -34,14 +30,14 @@ class DataIterator(StopIteration):
 
 
 class OneDimDataIterator(StopIteration):
-    def __init__(self, data: pd.DataFrame, element_length, shuffle=True, overlap=0, random_state=-1):
+    def __init__(self, data: pd.DataFrame, element_length, shuffle=True, overlap=0, random_state=None):
+        self.pseudo_random_gen = np.random.RandomState(random_state)
         self.data = data
         self.element_length = element_length
         unique_element_length = element_length - overlap
         sample_size = (data.shape[0] - overlap) // unique_element_length
         self.indexes = data.index.values.copy()
         self.position = 0
-        self.random_state = random_state
         self.labels = data.iloc[:, 0]
         self.labeled_indexes = dict()
         self.labeled_positions = dict()
@@ -49,25 +45,18 @@ class OneDimDataIterator(StopIteration):
             self.labeled_indexes[label] = self.labels[self.labels == 4].index.values.copy()
             self.labeled_positions[label] = 0
             if shuffle:
-                if random_state != -1:
-                    np.random.shuffle(self.labeled_indexes[label], random_state=random_state)
-                else:
-                    np.random.shuffle(self.labeled_indexes[label])
-        shuffle_set(self.indexes)
+                self.pseudo_random_gen.shuffle(self.labeled_indexes[label])
         if shuffle:
-            if random_state != -1:
-                np.random.shuffle(self.indexes, random_state=random_state)
-            else:
-                np.random.shuffle(self.indexes)
+            self.pseudo_random_gen.shuffle(self.indexes)
 
     def __iter__(self):
         return self
 
     def __next__(self):
-        class_label = choice(list(self.labeled_indexes.keys()))
+        class_label = self.pseudo_random_gen.choice(list(self.labeled_indexes.keys()))
         if class_label is not None:
             if self.labeled_positions[class_label] < len(self.labeled_indexes[class_label]) - 1:
-                start = np.random.randint(1, 599 - self.element_length)
+                start = self.pseudo_random_gen.randint(1, 599 - self.element_length)
                 self.labeled_positions[class_label] += 1
                 ret = pd.DataFrame({"ECoG_time": pd.RangeIndex(0, self.element_length),
                                     "ECoG_ch1": self.data.loc[self.labeled_indexes[class_label][self.labeled_positions[class_label]],
